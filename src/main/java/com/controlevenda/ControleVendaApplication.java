73440844e0b764e70d98e4c48566c7a2c913b659
Wende Mendes <wende@bluesoft.com.br>
2018-03-27 13:29:50 -0300

    refatorando

--- src/main/java/com/controlevenda/ControleVendaApplication.java
+++ src/main/java/com/controlevenda/ControleVendaApplication.java
@@ -2,13 +2,27 @@ package com.controlevenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ControleVendaApplication {
 
 	public static void main(String[] args) {
 		
-		
 		SpringApplication.run(ControleVendaApplication.class, args);
 	}
+	
+   	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+   	    return new WebMvcConfigurerAdapter() {
+		    @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
